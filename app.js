6d7a5687ce69926d8d93c64d44c0ed1c431ceb9c
nazmul6588764 <mdn344187@gmail.com>
2025-06-20 11:31:54 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-async function sendMessage() {
-  const input = document.getElementById("message-input");
-  const text = input.value;
-  const user = JSON.parse(localStorage.getItem("sunset_user"));
-  await supabase.from("messages").insert([{ text, room_id: "general", sender: user.email }]);
-  input.value = "";
-}
-supabase.channel('public:messages')
-  .on('postgres_changes', { event: '*', schema: 'public', table: 'messages' }, payload => {
-    const msg = payload.new;
-    document.getElementById("messages").innerHTML += `<div><b>${msg.sender}:</b> ${msg.text}</div>`;
-  }).subscribe();
+
